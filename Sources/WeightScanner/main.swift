66cb7956fc81f6a06270c8532cb50a14d2ef3cec
赤迫亮太 <>
2022-10-26 20:14:46 +0900

    通信テスト

--- Sources/WeightScanner/main.swift
+++ Sources/WeightScanner/main.swift
@@ -5,6 +5,10 @@
 //  Created by 赤迫亮太 on 2022/10/18.
 //
 
+import Foundation
+#if canImport(FoundationNetworking)
+import FoundationNetworking
+#endif
 import AWSLambdaRuntime
 
 private struct Request: Codable {}
@@ -13,7 +17,19 @@ private struct Response: Codable {
   let message: String
 }
 
+enum WeightScannerError: Swift.Error {
+    case dummy
+}
+
 Lambda.run { (context, request: Request, callback: @escaping (Result<Response, Error>) -> Void) in
-    let envValue = Lambda.env("TEST_ENV_KEY")
-    callback(.success(Response(message: "Hello, AWS Server! Environment value is \(envValue ?? "not found").")))
+    let url = URL(string: "https://www.google.com")!
+    let request = URLRequest(url: url)
+    let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+        guard let httpResponse = response as? HTTPURLResponse else {
+            callback(.failure(WeightScannerError.dummy))
+            return
+        }
+        callback(.success(Response(message: "statusCode: \(httpResponse.statusCode))")))
+    }
+    task.resume()
 }
